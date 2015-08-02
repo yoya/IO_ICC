9bff21fa384335facab9114fca826f3957758e4e
Yoshihiro Yamazaki <yoya@awm.jp>
2015-08-02 18:17:27 +0900

    ICC dump script.

---
+++ sample/iccdump.php
@@ -0,0 +1,38 @@
+<?php
+/*
+  ICC dump tool
+  (c) 2015/08/02- yoya@awm.jp
+*/
+
+require_once 'IO/ICC.php';
+
+$options = getopt("f:hd");
+
+function usage() {
+    echo "Usage: php iccdump.php [-h] [-d] -f <iccfile>".PHP_EOL;
+}
+
+if ((isset($options['f']) === false) ||
+    (is_readable($options['f']) === false)) {
+    usage();
+    exit(1);
+}
+$opts = array();
+
+if (isset($options['h'])) {
+  $opts['hexdump'] = true;
+}
+if (isset($options['d'])) {
+  $opts['detail'] = true;
+}
+
+
+$iccfile = $options['f'];
+$iccdata = file_get_contents($iccfile);
+
+$icc = new IO_ICC();
+$icc->parse($iccdata);
+
+$icc->dump($opts);
+
+exit(0);
