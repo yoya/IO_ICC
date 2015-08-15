0747a6a3e14f023b6f45f87f5693ac8ed5c1595c
Yoshihiro Yamazaki <yoya@awm.jp>
2015-08-15 22:06:19 +0900

    matrix と M curves の parse と dump 処理実装

--- IO/ICC/Type/MFAB.php
+++ IO/ICC/Type/MFAB.php
@@ -10,6 +10,7 @@ class IO_ICC_Type_MFAB extends IO_ICC_Type_Base {
     var $type = null;
     var $nInput, $nOutput;
     var $bCurve = null;
+    var $matrix = null;
     function __construct($iccInfo) {
         $this->_iccInfo = $iccInfo;
     }
@@ -28,6 +29,7 @@ class IO_ICC_Type_MFAB extends IO_ICC_Type_Base {
         $offsetToCLUT = $reader->getUI32BE();
         $offsetToACurve = $reader->getUI32BE();
         //        var_dump($offsetToBCurve, $offsetToMatrix, $offsetToMCurve, $offsetToCLUT, $offsetToACurve);
+        // B Curve
         $reader->setOffset($offsetToBCurve, 0);
         $bCurveContent = $reader->getData($offsetToMatrix - $offsetToBCurve);
         $bCurves = array();
@@ -41,17 +43,59 @@ class IO_ICC_Type_MFAB extends IO_ICC_Type_Base {
             $bCurveContent = substr($bCurveContent, $bCurve->getContentLength());
         }
         $this->bCurves = $bCurves;
-
+        // Matrix
+        $reader->setOffset($offsetToMatrix, 0);
+        $matrix = array();
+        for ($i = 0 ; $i < 12 ; $i++) {
+            $matrix []= $reader->getS15Fixed16Number();
+        }
+        $this->matrix = $matrix;
+        // M Curve
+        $reader->setOffset($offsetToMCurve, 0);
+        $mCurveContent = $reader->getData($offsetToMatrix - $offsetToMCurve);
+        $mCurves = array();
+        for ($i = 0 ; $i < $this->nOutput; $i++ ) {
+            $mCurve = IO_ICC_Type::makeType($mCurveContent, $this->_iccInfo);
+            if ($mCurve === false) {
+                break;
+            }
+            $mCurve->parseContent($mCurveContent);
+            $mCurves []= $mCurve;
+            $mCurveContent = substr($mCurveContent, $mCurve->getContentLength());
+        }
+        $this->mCurves = $mCurves;
     }
 
     function dumpContent($opts = array()) {
         $this->echoIndentSpace($opts);
         echo "nInput:{$this->nInput} nOutput:{$this->nOutput}".PHP_EOL;
+        $opts2 = array_merge($opts, array('level' => $opts['level']+1));
+        //
         $this->echoIndentSpace($opts);
         echo "bCurves:".PHP_EOL;
         foreach ($this->bCurves as $bCurve) {
-            $opts2 = array_merge($opts, array('level' => $opts['level']+1));
+            $this->echoIndentSpace($opts2);
+            echo "Type:{$bCurve->type}\n";
             $bCurve->dumpContent($opts2);
+        }
+        //
+        $this->echoIndentSpace($opts);
+        echo "Matrix:".PHP_EOL;
+        for ($y = 0 ; $y < 3 ; $y++) {
+            $this->echoIndentSpace($opts);
+            for ($x = 0 ; $x < 3 ; $x++) {
+                printf("  %2.4f", $this->matrix[$x + $y*3]+9);
+            }
+            printf("    %2.4f", $this->matrix[9 + $y]);
+            echo PHP_EOL;
+        }
+        //
+        $this->echoIndentSpace($opts);
+        echo "mCurves:".PHP_EOL;
+        foreach ($this->mCurves as $mCurve) {
+            $this->echoIndentSpace($opts2);
+            echo "Type:{$mCurve->type}\n";
+            $mCurve->dumpContent($opts2);
         }
     }
 
