55b332b6df743c12df966f1ec3283b720d919fcb
Yoshihiro Yamazaki <yoya@awm.jp>
2015-09-20 22:44:03 +0900

    タグのオフセットを 4 の倍数でアライメントを取った。(ImageMagick の中の ICC ファイルがそうだったので)

--- IO/ICC/Tag.php
+++ IO/ICC/Tag.php
@@ -43,6 +43,11 @@ class IO_ICC_Tag {
         $type = $this->type;
         if (is_null($this->content)) {
             $this->content = $this->buildTagContent();
+            $alighSize = 4;
+            $remainder = strlen($this->content) % $alighSize;
+            if ($remainder) {
+                $this->content .= str_pad("\0", $alighSize - $remainder);
+            }
         }
         return $this->content;
     }
