86f7e5a9bd2d736c3d807ea9c5011078d08faa2d
Yoshihiro Yamazaki <yoya@awm.jp>
2015-09-23 03:19:36 +0900

    alltest で進捗表示をつけた。最後に build を追加。

--- test/alltest.php
+++ test/alltest.php
@@ -23,14 +23,17 @@ while (($file = readdir($dh)) !== false) {
     echo "$path".PHP_EOL;
     $iccdata = file_get_contents($path);
 
+    echo "    test1.".PHP_EOL;
     $icc1 = new IO_ICC_Editor();
     $icc1->parse($iccdata);
     ob_start('null_callback');
     $icc1->dump();
     ob_end_clean();
     $iccdata2 = $icc1->build();
-    $iccdata3 = $icc1->rebuild();
+    $icc1->rebuild();
+    $iccdata3 = $icc1->build();
     //
+    echo "    test2.".PHP_EOL;
     $icc2 = new IO_ICC_Editor();
     $icc2->parse($iccdata2);
     ob_start('null_callback');
@@ -38,7 +41,9 @@ while (($file = readdir($dh)) !== false) {
     ob_end_clean();
     $icc2->build();
     $icc2->rebuild();
+    $icc2->build();
     //
+    echo "    test3.".PHP_EOL;
     $icc3 = new IO_ICC_Editor();
     $icc3->parse($iccdata3);
     ob_start('null_callback');
@@ -46,6 +51,7 @@ while (($file = readdir($dh)) !== false) {
     ob_end_clean();
     $icc3->build();
     $icc3->rebuild();
+    $icc3->build();
 }
 
 exit (0);
